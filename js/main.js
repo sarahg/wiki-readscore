7c4df9fa838a072565aa156058cc765f69b3e128
Sarah German <sarah@advomatic.com>
2017-05-28 10:31:07 -0500

    Log request time to console.

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ $(document).ready(function() {
     // @todo maybe leave the form in place and show the loader below instead
     $(this).html('<div class="loader">Loading...</div>');
 
+    //var start_time = new Date().getTime();
+
     $.ajax({
       type: 'POST',
       url: 'includes/app.php',
@@ -17,6 +19,9 @@ $(document).ready(function() {
       success: function(result) {
         $('.loader').hide();
         $('.results').html(result);
+
+        //var request_time = new Date().getTime() - start_time;
+        //console.log('Request time: ' + request_time + 'ms');
 
         // @todo table sortability by title/score
         // @todo maybe add fancy classes to rows to have diff colors for score ranges (red/yellow/green)
